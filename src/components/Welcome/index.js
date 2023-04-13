cd8350985ef710b7c48db4c8ae1cd0406031a971
ashikumari98 <ashmirtikumari18@gmail.com>
2023-04-13 13:29:34 +0000

    Welcome-solution

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,42 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: true}
+
+  changeBtn = () => {
+    const {isSubscribed} = this.state
+
+    if (isSubscribed === true) {
+      this.setState({isSubscribed: false})
+    } else {
+      this.setState({isSubscribed: true})
+    }
+  }
+
+  render() {
+    const {isSubscribed} = this.state
+    return (
+      <div className="container">
+        <div>
+          <h1 className="heading">Welcome</h1>
+          <p className="description">Thank you! Happy Learning</p>
+          {isSubscribed ? (
+            <button type="button" onClick={this.changeBtn} className="button">
+              Subscribe
+            </button>
+          ) : (
+            <button type="button" onClick={this.changeBtn} className="button">
+              Subscribed
+            </button>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Welcome
